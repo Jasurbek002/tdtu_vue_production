b1f9f956d41e9fd8b3632d6d1d1ac05f03f7e70e
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-06-01 15:13:13 +0500

    lang and thema finish

---
+++ assets/ds-c2a66a50.js
@@ -0,0 +1,1 @@
+import{m as r}from"./index-417ce120.js";const t={};function c(e,n){return"Ds doctarant"}const o=r(t,[["render",c]]);export{o as default};
--- assets/support-c8107434.js
+++
@@ -1,1 +0,0 @@
-import{m as r}from"./index-b4f090a6.js";const t={};function c(n,a){return" Tayanch doctarant "}const o=r(t,[["render",c]]);export{o as default};
