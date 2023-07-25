98f924cacf875513901de8c3db429b041dc2afcc
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-07-25 16:44:08 +0500

    climate addd

---
+++ assets/Objects-72f5a3f1.js
@@ -0,0 +1,1 @@
+import{d as e,o as a,e as o,_ as s}from"./index-e0a06c81.js";const r=e({__name:"Objects",setup(t){return(_,c)=>(a(),o(s,{label:"INTELEKTUAL MULK OBYEKTLARI"}))}});export{r as default};
---
+++ assets/inno-0e79040d.js
@@ -0,0 +1,1 @@
+import{y as c,o as e,c as n}from"./index-e0a06c81.js";const o={};function r(t,s){return e(),n("div",null," doc ")}const _=c(o,[["render",r]]);export{_ as default};
