37a1b79b07c753c79b004a73502a22299d4e0673
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-11-06 18:06:41 +0500

    pudhhhhh

--- assets/Greenmetric-806f91a7.js
+++
@@ -1,6 +0,0 @@
-import{_ as e}from"./Row.vue_vue_type_script_setup_true_lang-db05c54c.js";import{_ as i}from"./List.vue_vue_type_script_setup_true_lang-0ec651e0.js";import{d as r,o as a,b as n,w as t,g as o,e as s}from"./index-8be0a5e5.js";const l="/assets/green-78c18ce4.png",f=r({__name:"Greenmetric",setup(c){return(g,m)=>(a(),n(e,{label:"Greenmetric"},{default:t(()=>[o(i,{image:s(l),title:" Toshkent davlat texnika universiteti “Yashil reytingi “da",text:`Joriy yilning 12 – dekabrida “Greenmetric ning\r
-                    jahon universitetlarining ekologiya va atrof-muhit barqarorligi boʻyicha\r
-                    reytingini eʼlon qildi. Toshkent davlat texnika universiteti\r
-                    ilk bor xalqaro yashil universitetlar reytingida\r
-                    ishtirok etib “Greenmetric” reyting mezonlarida\r
-                    quyidagi natijalarga erishildi:`,styles:"bg-grenn"},null,8,["image"])]),_:1}))}});export{f as default};
