d80cea6bfdd7bf4e23e0acf5aa1510f6ac27f038
jasurbek <94533241+Jasurbek002@users.noreply.github.com>
2023-12-12 12:19:51 +0500

    email updated

---
+++ assets/inno-0cc2d9eb.js
@@ -0,0 +1,1 @@
+import{d as e,b as t,o as n}from"./index-8e22b219.js";const o={class:"w-full h-[80vh] flex justify-center items-center"},i=e({__name:"inno",setup(c){return window.location.href="https://www.innotechnopark.com/",(s,r)=>(n(),t("div",o," INNO TEXNOPARK "))}});export{i as default};
